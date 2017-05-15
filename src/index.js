890f10e2763eb19fd1cf8e5a880be5ada2042aec
Noah Muldavin <nmuldavin@gmail.com>
2017-05-14 21:43:13 -0700

    deep equlity complete

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ const bothNumbers = (left, right) => isNumber(right) && isNumber(left)
 
 const almostEqual = (left, right, tol) => Math.abs(left - right) <= tol
 
-const chaiAlmost = (standardTolerance = DEFAULT_TOLERANCE) => ({ Assertion }, { flag }) => {
-  function customComparator (left, right) {
-    if (bothNumbers(left, right)) {
-      return almostEqual(left, right, standardTolerance)
-    }
-    return null
+const comparator = tolerance => (left, right) => {
+  if (bothNumbers(left, right)) {
+    return almostEqual(left, right, tolerance)
   }
+  return null
+}
 
+const chaiAlmost = (standardTolerance = DEFAULT_TOLERANCE) => ({ Assertion }, { flag }) => {
   function method (expected, customTolerance = standardTolerance) {
     return this.closeTo(expected, customTolerance)
   }
@@ -53,12 +53,36 @@ const chaiAlmost = (standardTolerance = DEFAULT_TOLERANCE) => ({ Assertion }, { 
     }
   }
 
+  function assertEql (_super) {
+    return function (val, msg) {
+      if (msg) flag(this, 'message', msg)
+
+      const tolerance = flag(this, 'tolerance')
+
+      if (tolerance) {
+        this.assert(
+          deepEqual(val, this._obj, { comparator: comparator(tolerance) }),
+          'expected #{this} to deeply almost equal #{exp}',
+          'expected #{this} to not deeply almost equal #{exp}',
+          val,
+          this._obj,
+          true
+        )
+      } else {
+        return _super.apply(this, arguments)
+      }
+    }
+  }
+
   Assertion.addMethod('almost', method)
   Assertion.addChainableMethod('almost', customChainingBehavior, defaultChainingBehavior)
 
   Assertion.overwriteMethod('equal', assertEqual)
   Assertion.overwriteMethod('equals', assertEqual)
   Assertion.overwriteMethod('eq', assertEqual)
+
+  Assertion.overwriteMethod('eql', assertEql)
+  Assertion.overwriteMethod('eqls', assertEql)
 }
 
 module.exports = chaiAlmost
--- test/index.js
+++ test/index.js
@@ -21,21 +21,29 @@ describe('chaiAlmost', () => {
   describe('almost chainable method', () => {
     describe('with shallow equality', () => {
       it('should modify shallow equality checks on numbers to allow default tolerance', () => {
-        expect(3.9999999).to.almost.equal(4)
-        expect(3.9999999).to.almost.eq(4)
-        expect(3.9999999).almost.equals(4)
-        expect(3.9).to.not.almost.equal(4)
-        expect(3.9).to.not.almost.eq(4)
-        expect(3.9).not.almost.equals(4)
+        const exp = 4
+        const good = 3.9999999
+        const bad = 3.9
+
+        expect(good).to.almost.equal(exp)
+        expect(good).to.almost.eq(exp)
+        expect(good).almost.equals(exp)
+        expect(bad).to.not.almost.equal(exp)
+        expect(bad).to.not.almost.eq(exp)
+        expect(bad).not.almost.equals(exp)
       })
 
       it('should modify shallow equality checks on numbers to allow custom tolerance', () => {
-        expect(3.8).to.almost(1).equal(4)
-        expect(3.8).to.almost(1).eq(4)
-        expect(3.8).almost(1).equals(4)
-        expect(2).to.not.almost(1).equal(4)
-        expect(2).to.not.almost(1).eq(4)
-        expect(2).not.almost(1).equals(4)
+        const exp = 4
+        const good = 3.8
+        const bad = 2
+
+        expect(good).to.almost(1).equal(exp)
+        expect(good).to.almost(1).eq(exp)
+        expect(good).almost(1).equals(exp)
+        expect(bad).to.not.almost(1).equal(exp)
+        expect(bad).to.not.almost(1).eq(exp)
+        expect(bad).not.almost(1).equals(exp)
       })
 
       it('should not modify shallow equality checks on non-numbers', () => {
@@ -47,6 +55,47 @@ describe('chaiAlmost', () => {
         expect(x).to.not.almost.equal({ taco: 'pastor' })
         expect(y).to.almost.equal(y)
         expect(y).to.not.almost.equal([1, 2, 3])
+      })
+    })
+
+    describe('with deep equality', () => {
+      it('should modify deep equality checks to allow default tolerance for numbers', () => {
+        const exp = { taco: 'pastor', quantity: 3 }
+        const good = { taco: 'pastor', quantity: 2.9999999 }
+        const bad = { taco: 'pastor', quantity: 3.1 }
+
+        expect(good).to.almost.eql(exp)
+        expect(good).to.deep.almost.equal(exp)
+        expect(good).almost.deep.eqls(exp)
+        expect(bad).to.not.almost.eql(exp)
+        expect(bad).to.not.deep.almost.equal(exp)
+        expect(bad).not.almost.deep.eqls(exp)
+      })
+
+      it('should modify deep equality checks to allow custom tolerance for numbers', () => {
+        const exp = { taco: 'pastor', quantity: 10 }
+        const good = { taco: 'pastor', quantity: 29 }
+        const bad = { taco: 'pastor', quantity: 31 }
+
+        expect(good).to.almost(20).eql(exp)
+        expect(good).to.deep.almost(20).equal(exp)
+        expect(good).almost(20).deep.eqls(exp)
+        expect(bad).to.not.almost(20).eql(exp)
+        expect(bad).to.not.deep.almost(20).equal(exp)
+        expect(bad).not.almost(20).deep.eqls(exp)
+      })
+
+      it('should not modify shallow equality checks on non-numbers', () => {
+        const exp = { taco: 'pastor', delicious: true }
+        const good = { taco: 'pastor', delicious: true }
+        const bad = { taco: 'pastor', delicious: false }
+
+        expect(good).to.almost.eql(exp)
+        expect(good).to.deep.almost.equal(exp)
+        expect(good).almost.deep.eqls(exp)
+        expect(bad).to.not.almost.eql(exp)
+        expect(bad).to.not.deep.almost.equal(exp)
+        expect(bad).not.almost.deep.eqls(exp)
       })
     })
   })
